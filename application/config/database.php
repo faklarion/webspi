d087ccb013a2fbb6092183fa3eafc74ca766dc7b
faklarion <faisal.hazard17@gmail.com>
2024-10-16 14:28:50 +0800

    setting untuk localhost

--- application/config/database.php
+++ application/config/database.php
@@ -76,9 +76,9 @@ $query_builder = TRUE;
 $db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
-	'username' => 'gskstore_spi2',
-	'password' => ',x-^hl.}=ul&',
-	'database' => 'gskstore_spi2',
+	'username' => 'root',
+	'password' => '',
+	'database' => 'webspi',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
